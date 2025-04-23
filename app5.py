55c9eb6234496a92fd6d2ec3a8da6c3e235d2231
earltavera <earl_tavera@yahoo.com>
2025-04-23 21:32:48 +1200

    Update app5.py

--- app5.py
+++ app5.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
-import fitz  # PyMuPDF
+import PyMuPDF  # PyMuPDF
 from io import BytesIO
 from datetime import datetime, timedelta
 import base64
