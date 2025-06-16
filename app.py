b0cf640dbedd52d238946afc3b10c6c143445bec
earltavera <earl_tavera@yahoo.com>
2025-06-16 18:20:53 +1200

    Update app.py

--- app.py
+++ app.py
@@ -373,7 +373,7 @@ def extract_metadata(text):
         "AUP(OP) Triggers": triggers_str if triggers_str else "Unknown AUP Triggers",
         "Reason for Consent": proposal_str if proposal_str else "Unknown Reason for Consent",
         "Consent Condition Number": ", ".join(conditions_numbers) if conditions_numbers else "Unknown Consent Conditions",
-        "Consent Conditions": ", ".join() if managementplan_final else "None",
+        "Consent Conditions": ", ".join() if Consent_conditions else "None",
         "Consent Status": check_expiry(expiry_date), # This will now use the localized date
         "Text Blob": text
     }
